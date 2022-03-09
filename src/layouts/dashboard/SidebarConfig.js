920adcc5d5e48a6c5e9d11c67252491f686f9cf5
lucas-goldner <lucas.goldner@googlemail.com>
2022-03-09 21:52:58 +0100

    feat: Remove all components I am not using

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -7,39 +7,9 @@ const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
 
 const sidebarConfig = [
   {
-    title: 'dashboard',
-    path: '/dashboard/app',
-    icon: getIcon('eva:pie-chart-2-fill')
-  },
-  {
-    title: 'user',
-    path: '/dashboard/user',
-    icon: getIcon('eva:people-fill')
-  },
-  {
     title: 'events',
     path: '/dashboard/events',
     icon: getIcon('eva:swap-outline')
-  },
-  {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: getIcon('eva:shopping-bag-fill')
-  },
-  {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: getIcon('eva:file-text-fill')
-  },
-  {
-    title: 'login',
-    path: '/login',
-    icon: getIcon('eva:lock-fill')
-  },
-  {
-    title: 'register',
-    path: '/register',
-    icon: getIcon('eva:person-add-fill')
   },
   {
     title: 'Not found',
